e4f18a281c0dd855d05847951689feeee9342850
pranav <pranav15197@gmail.com>
2017-06-14 18:20:27 +0530

    added view

--- app/src/main/java/auth/view/LoginActivity.java
+++ app/src/main/java/auth/view/LoginActivity.java
@@ -2,14 +2,45 @@ package auth.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.sbs.gp.sbsgpandroidapp.R;
 
+import auth.viewmodel.LoginViewModel;
+import rx.subscriptions.CompositeSubscription;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mobile;
+    private Button requestOTPButton;
+
+    private CompositeSubscription subscriptions = new CompositeSubscription();
+    private LoginViewModel loginViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        bindViews();
+    }
+
+    private void bindViews() {
+
+        mobile = (EditText)findViewById(R.id.mobile);
+        requestOTPButton = (Button)findViewById(R.id.request_otp_btn);
+        requestOTPButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestOTP();
+            }
+        });
+
+    }
+
+    private void requestOTP() {
+
     }
 }
---
+++ app/src/main/java/auth/viewmodel/LoginViewModel.java
@@ -0,0 +1,8 @@
+package auth.viewmodel;
+
+/**
+ * Created by pranav on 14/06/17.
+ */
+
+public class LoginViewModel {
+}
